0a1350e41ed0fdc2834aa0479be3890357ff3aca
danilo-89 <danilo89abcd@gmail.com>
2021-05-23 22:55:06 +0200

    new changes

--- 1/part1/src/App.js
+++ 1/part1/src/App.js
@@ -1,32 +1,134 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Hello = (props) => {
+
+// const Display = ({ counter }) => {
+//   return (
+//     <div>{counter}</div>
+//   );
+// }
+
+// const Buttons = ({ handleClick, text }) => {
+//   return (
+//     <button onClick={handleClick}>
+//       {text}
+//     </button>
+//   );
+// }
+
+// const App = () => {
+//   const [counter, setCounter] = useState(0)
+
+//   const increaseByOne = () => setCounter(counter + 1);
+//   const decreaseByOne = () => setCounter(counter - 1);
+//   const setToZero = () => setCounter(0);
+
+
+//   console.log('rendering...', counter)
+
+//   return (
+//     <>
+//       <Display counter={counter} />
+//       <Buttons
+//         handleClick={setToZero}
+//         text='zero'
+//       />
+//       <Buttons
+//         handleClick={increaseByOne}
+//         text='plus'
+//       />
+//       <Buttons
+//         handleClick={decreaseByOne}
+//         text='minus'
+//       />
+//     </>
+//   )
+// }
+
+// --------------------------------------------------------
+// --------------------------------------------------------
+
+// const History = (props) => {
+//   if (props.allClicks.length < 1) {
+//     return ( 
+//       <div>
+//         the app is used by clicking buttons
+//       </div>
+//      );
+//   } else {
+//     return ( 
+//       <div>
+//         Buttons press history :{props.allClicks.join(' ')}
+//       </div>
+//     )
+//   }
+// }
+
+// const Button = (props) => {
+//   console.log('props value is', props)
+
+//   return (
+//     <button onClick={props.onClick}>
+//       {props.text}
+//     </button>
+//   )  
+// }
+
+
+// const App = () => {
+//   const [left, setLeft] = useState(0)
+//   const [right, setRight] = useState(0)
+//   const [allClicks, setAll] = useState([])
+
+//   const handleLeftClick = () => {
+//     setAll(allClicks.concat('L'))
+//     setLeft(left + 1)
+//   }
+
+//   const handleRightClick = () => {
+//     setAll(allClicks.concat('R'))
+//     setRight(right + 1)
+//   }
+
+//   return (
+//     <div>
+//       {left}
+//       <Button onClick={handleLeftClick} text='left' />
+//       <Button onClick={handleRightClick} text='right' />
+//       {right}
+//       <History allClicks={allClicks} />
+//     </div>
+//   )
+// }
+
+const Display = (props) => {
   return ( 
     <div>
-      <p>Hello {props.name || "test"}, you are {props.age || 0} years old</p>
+      {props.value}
     </div>
   );
 }
 
-const Footer = () => {
-  return ( 
-    <div>
-      greeting app created by <a href="https://github.com/mluukkai">mluukkai</a>
-    </div>
+const Button = (props) => {
+  return (
+    <button onClick={props.onClick}>
+      {props.text}
+    </button>
   );
 }
-
 
 const App = () => {
+  const [value, setValue] = useState(10)
+
+  const setToValue = (newValue) => {
+    setValue(newValue)
+  }
 
   return (
-    <>
-    <h1>Greetings</h1>
-    <Hello name='George' age={45 - 15} />
-    <Footer />
-    </>
-    )
-  
+    <div>
+      <Display value={value} />
+      <Button onClick={() => setToValue(1000)} text={"Button"}></Button>
+    </div>
+  )
 }
 
 export default App
