879f2ed1ca0f3f5962bc85585ba9dc8820bc91fe
danilo-89 <danilo89abcd@gmail.com>
2023-12-28 14:21:16 +0100

    refactor and add features

--- 3/index.js
+++ 3/index.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
-
+const Person = require('./models/person');
 const app = express();
-
 app.use(express.json());
 app.use(cors());
 app.use(express.static('dist'));
@@ -26,38 +25,9 @@ const morganConfig = (tokens, req, res) => {
 
 app.use(morgan(morganConfig));
 
-// Utils
-const generateId = () => {
-	return Math.floor(Math.random() * 999999999999999);
-};
-
-// Data
-let persons = [
-	{
-		id: 1,
-		name: 'Arto Hellas',
-		number: '040-123456',
-	},
-	{
-		id: 2,
-		name: 'Ada Lovelace',
-		number: '39-44-5323523',
-	},
-	{
-		id: 3,
-		name: 'Dan Abramov',
-		number: '12-43-234345',
-	},
-	{
-		id: 4,
-		name: 'Mary Poppendieck',
-		number: '39-23-6423122',
-	},
-];
-
 // API Endpoints
-app.get('/info', (request, response) => {
-	const totalEntries = persons.length;
+app.get('/info', async (request, response) => {
+	const totalEntries = await Person.estimatedDocumentCount();
 	const dateNow = new Date();
 
 	response.send(`<p>
@@ -67,58 +37,93 @@ app.get('/info', (request, response) => {
   </p>`);
 });
 
-app.get('/api/persons', (request, response) => {
-	response.json(persons);
+app.get('/api/persons', (request, response, next) => {
+	Person.find({})
+		.then((persons) => {
+			response.json(persons);
+		})
+		.catch((error) => {
+			next(error);
+		});
 });
 
-app.get('/api/persons/:id', (request, response) => {
-	const id = Number(request.params.id);
-	const person = persons.find((item) => item.id === id);
-	if (person) {
-		response.json(person);
-	} else {
-		response.status(404).end();
-	}
+app.get('/api/persons/:id', (request, response, next) => {
+	Person.findById(request.params.id)
+		.then((person) => {
+			if (person) {
+				response.json(person);
+			} else {
+				response.status(404).end();
+			}
+		})
+		.catch((error) => {
+			next(error);
+		});
 });
 
-app.delete('/api/persons/:id', (request, response) => {
-	const id = Number(request.params.id);
-	persons = persons.filter((item) => item.id !== id);
-
-	response.status(204).end();
+app.delete('/api/persons/:id', (request, response, next) => {
+	Person.findByIdAndDelete(request.params.id)
+		.then(() => {
+			response.status(204).end();
+		})
+		.catch((error) => next(error));
 });
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', async (request, response, next) => {
 	const content = request.body;
 
-	if (!content) {
-		return response.status(400).json({
-			error: 'content missing',
-		});
-	}
+	const personData = {
+		name: content.name,
+		number: content.number,
+	};
 
-	if (!content.name || !content.number) {
-		return response.status(400).json({
-			error: 'name or number missing',
-		});
-	}
+	const isExistingPerson = await Person.exists({ name: content.name });
 
-	if (persons.findIndex((item) => item.name === content.name) > -1) {
+	if (isExistingPerson) {
 		return response.status(400).json({
 			error: 'name must be unique',
 		});
 	}
 
-	const person = {
+	Person.create(personData)
+		.then((result) => {
+			response.json(result);
+		})
+		.catch((error) => next(error));
+});
+
+app.put('/api/persons/:id', (request, response, next) => {
+	const content = request.body;
+
+	const personData = {
 		name: content.name,
 		number: content.number,
-		id: generateId(),
 	};
 
-	persons = persons.concat(person);
-
-	response.json(person);
+	Person.findByIdAndUpdate(request.params.id, personData, {
+		new: true,
+		runValidators: true,
+		context: 'query',
+	})
+		.then((updatedPerson) => {
+			response.json(updatedPerson);
+		})
+		.catch((error) => next(error));
 });
+
+// Handler of requests with result to errors
+const errorHandler = (error, request, response, next) => {
+	console.error(error.message);
+
+	if (error.name === 'CastError') {
+		return response.status(400).send({ error: 'malformatted id' });
+	} else if (error.name === 'ValidationError') {
+		return response.status(400).json({ error: error.message });
+	}
+
+	next(error);
+};
+app.use(errorHandler);
 
 // Configure and Start the Server
 const PORT = 3001;
